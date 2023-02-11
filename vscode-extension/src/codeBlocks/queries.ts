407623cdd62e3c54a4de5969b2ef4862ac174659
selfint <selfint@gmail.com>
2023-02-11 20:13:50 +0200

    extract queries to json file

--- vscode-extension/src/codeBlocks/queries.ts
+++ vscode-extension/src/codeBlocks/queries.ts
@@ -1,136 +1,17 @@
 import { SupportedLanguage } from "./types";
+import queriesStringsJson from "./queriesStrings.json";
 
 export function getQueryStrings(lang: SupportedLanguage): string[] {
   switch (lang) {
     case "rust":
-      return [
-        `
-      (
-          (
-              [
-                  (attribute_item)
-                  (line_comment)
-              ] @header
-              .
-              [
-                  (attribute_item)
-                  (line_comment)
-              ]* @header
-          )?
-          .
-          (function_item) @item
-      )
-      `,
-        `
-      (
-          (
-              [
-                  (attribute_item)
-                  (line_comment)
-              ] @header
-              .
-              [
-                  (attribute_item)
-                  (line_comment)
-              ]* @header
-          )?
-          .
-          (mod_item) @item
-      )
-      `,
-        `
-      (
-          (
-              [
-                  (attribute_item)
-                  (line_comment)
-              ] @header
-              .
-              [
-                  (attribute_item)
-                  (line_comment)
-              ]* @header
-          )?
-          .
-          (struct_item) @item
-      )
-      `,
-        `(impl_item) @item`,
-      ];
+      return queriesStringsJson["rust"];
     case "typescript":
-      return [
-        `
-(
-  (comment)* @header
-  .
-  (class_declaration) @item
-)
-        `,
-        `
-(
-  (comment)* @header
-  .
-  (method_definition) @item
-)
-        `,
-        `
-(
-  (comment)* @header
-  .
-  (function_declaration) @item
-)
-        `,
-        `
-(
-  (comment)* @header
-  .
-  (export_statement) @item
-)
-        `,
-      ];
+      return queriesStringsJson["typescript"];
     case "tsx":
-      const typescriptQueries = getQueryStrings("typescript");
-      const tsxQueries = [`(jsx_element) @item`, "(jsx_self_closing_element) @item"];
-      return [...typescriptQueries, ...tsxQueries];
+      return [...queriesStringsJson["typescript"], ...queriesStringsJson["tsx"]];
     case "typescriptreact":
       return getQueryStrings("tsx");
     case "svelte":
-      return [
-        `
-(
-  (comment)* @header
-  .
-  (element) @item
-)
-        `,
-        `
-(
-  (comment)* @header
-  .
-  (each_statement) @item
-)
-        `,
-        `
-(
-  (comment)* @header
-  .
-  (if_statement) @item
-)
-        `,
-        `
-(
-  (comment)* @header
-  .
-  (style_element) @item
-)
-        `,
-        `
-(
-  (comment)* @header
-  .
-  (script_element) @item
-)
-        `,
-      ];
+      return queriesStringsJson["svelte"];
   }
 }
