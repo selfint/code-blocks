3129d296ac478bcc38289e12835948f872c62ad5
selfint <selfint@gmail.com>
2023-02-11 20:04:32 +0200

    Improve ui look

--- vscode-extension/webview-ui/src/Tree.svelte
+++ vscode-extension/webview-ui/src/Tree.svelte
@@ -1,86 +1,81 @@
 <script lang="ts">
+  import { children } from "svelte/internal";
   import type { BlockLocation, BlockLocationTree } from "./types";
 
   export let text: string;
   export let tree: BlockLocationTree;
   export let onClickHandler: (block: BlockLocation) => void;
-  export let selected: BlockLocation | undefined;
+  export let selectedBlock: BlockLocation | undefined;
 
-  $: blockSelected = tree.block === selected;
-  $: borderColor = blockSelected ? "var(--vscode-focusBorder)" : "var(--vscode-input-border)";
+  const selectedColor = "var(--vscode-editor-selectionBackground)";
+  const defaultColor = "var(--vscode-editor-background)";
 
-  const substringSizeLimit = 100;
-  const expandOnClick = false;
+  $: backgroundColor = tree.block === selectedBlock ? selectedColor : defaultColor;
 
-  function getSubstring(start: number, end: number, blockSelected: boolean): string {
-    if (end - start < substringSizeLimit || (blockSelected && expandOnClick)) {
+  const sliceLengthLimit = 100;
+
+  function textSlice(start: number, end: number): string {
+    if (end - start < sliceLengthLimit) {
       return text.substring(start, end);
     } else {
       return (
-        text.substring(start, start + substringSizeLimit / 2) +
+        text.substring(start, start + sliceLengthLimit / 2) +
         "..." +
-        text.substring(end - substringSizeLimit / 2, end)
+        text.substring(end - sliceLengthLimit / 2, end)
       );
     }
   }
 </script>
 
-<div
-  class="block"
-  style="border-color: {borderColor}"
-  on:click|self|preventDefault={() => onClickHandler(tree.block)}
-  on:keypress|self|preventDefault={() => onClickHandler(tree.block)}
->
-  {#if tree.children.length !== 0}
-    <div
-      on:click|self|preventDefault={() => onClickHandler(tree.block)}
-      on:keypress|self|preventDefault={() => onClickHandler(tree.block)}
-    >
-      {getSubstring(tree.block.start_byte, tree.children[0].block.start_byte, blockSelected)}
-    </div>
-    {#each tree.children as childTree, i}
-      <svelte:self {text} tree={childTree} {onClickHandler} {selected} />
-      {#if i !== tree.children.length - 1}
-        <div
-          on:click|self|preventDefault={() => onClickHandler(tree.block)}
-          on:keypress|self|preventDefault={() => onClickHandler(tree.block)}
-        >
-          {getSubstring(
-            tree.children[i].block.end_byte,
-            tree.children[i + 1].block.start_byte,
-            blockSelected
-          )}
-        </div>
-      {/if}
-    {/each}
-    <div
-      on:click|self|preventDefault={() => onClickHandler(tree.block)}
-      on:keypress|self|preventDefault={() => onClickHandler(tree.block)}
-    >
-      {getSubstring(
-        tree.children[tree.children.length - 1].block.end_byte,
-        tree.block.end_byte,
-        blockSelected
-      )}
-    </div>
-  {:else}
-    {getSubstring(tree.block.start_byte, tree.block.end_byte, blockSelected)}
-  {/if}
-</div>
+<main>
+  <div
+    class="block"
+    style="background-color: {backgroundColor}"
+    on:click|self|preventDefault={() => onClickHandler(tree.block)}
+    on:keypress|self|preventDefault={() => onClickHandler(tree.block)}
+  >
+    {#if tree.children.length !== 0}
+      <div
+        on:click|self|preventDefault={() => onClickHandler(tree.block)}
+        on:keypress|self|preventDefault={() => onClickHandler(tree.block)}
+      >
+        {textSlice(tree.block.start_byte, tree.children[0].block.start_byte)}
+      </div>
+      {#each tree.children as childTree, i}
+        <svelte:self {text} tree={childTree} {onClickHandler} {selectedBlock} />
+        {#if i !== tree.children.length - 1}
+          <div
+            on:click|self|preventDefault={() => onClickHandler(tree.block)}
+            on:keypress|self|preventDefault={() => onClickHandler(tree.block)}
+          >
+            {textSlice(tree.children[i].block.end_byte, tree.children[i + 1].block.start_byte)}
+          </div>
+        {/if}
+      {/each}
+      <div
+        on:click|self|preventDefault={() => onClickHandler(tree.block)}
+        on:keypress|self|preventDefault={() => onClickHandler(tree.block)}
+      >
+        {textSlice(tree.children.at(-1).block.end_byte, tree.block.end_byte)}
+      </div>
+    {:else}
+      {textSlice(tree.block.start_byte, tree.block.end_byte)}
+    {/if}
+  </div>
+</main>
 
 <style>
   .block {
-    margin: 5px;
-    text-align: left;
-    padding: 5px;
-    border-color: var(--vscode-input-border);
-    border-width: 1px;
+    background-color: var(--vscode-editor-background);
+    border-color: var(--vscode-editorIndentGuide-background);
     border-style: solid;
-    display: block;
-    color: var(--vscode-input-foreground);
-    outline-color: var(--vscode-input-border);
+    border-width: 1px;
+    color: var(--vscode-editor-foreground);
+    font-family: var(--vscode-font-family);
     font-size: var(--vscode-font-size);
     font-weight: var(--vscode-font-weight);
-    font-family: var(--vscode-font-family);
+    margin: 5px;
+    padding: 5px;
+    text-align: left;
   }
 </style>
