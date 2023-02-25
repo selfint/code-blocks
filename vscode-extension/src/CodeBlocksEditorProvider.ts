acaacc8abf676328394c09a99c678a56209c87ef
selfint <selfint@gmail.com>
2023-02-25 13:56:53 +0200

    extract uiBridge

--- vscode-extension/src/CodeBlocksEditorProvider.ts
+++ vscode-extension/src/CodeBlocksEditorProvider.ts
@@ -2,19 +2,10 @@ import * as vscode from "vscode";
 import * as path from "path";
 import { getNonce } from "./utilities/getNonce";
 import { getUri } from "./utilities/getUri";
-import * as codeBlocksCliClient from "./codeBlocks/codeBlocksCliClient";
-import {
-  BlockLocation,
-  GetSubtreesArgs,
-  GetSubtreesResponse,
-  JsonResult,
-  MoveBlockArgs,
-  MoveBlockResponse,
-  SupportedLanguage,
-} from "./codeBlocks/types";
-import { getQueryStrings } from "./codeBlocks/queries";
-import { MoveCommand, UpdateMessage } from "./messages";
+import { SupportedLanguage } from "./codeBlocks/types";
+import { MoveCommand } from "./messages";
 import { getOrInstallCli } from "./codeBlocks/installer/installer";
+import { moveBlock, updateUiBlocks } from "./uiBridge";
 
 const vscodeLangIdToSupportedLanguage: Map<string, SupportedLanguage> = new Map([
   ["svelte", "svelte"],
@@ -36,11 +27,7 @@ export class CodeBlocksEditorProvider implements vscode.CustomTextEditorProvider
   }
 
   async handleMessage(document: vscode.TextDocument, message: MoveCommand): Promise<void> {
-    switch (message.command) {
-      case "move":
-        await this.handleMoveCommand(message.args, document);
-        break;
-    }
+    await moveBlock(message, document, this.docLang!, this.binPath!);
   }
 
   public async resolveCustomTextEditor(
@@ -67,7 +54,7 @@ export class CodeBlocksEditorProvider implements vscode.CustomTextEditorProvider
 
     this.subscribeToDocEvents(webviewPanel, document);
 
-    await this.updateWebview(document, webviewPanel);
+    await updateUiBlocks(document, webviewPanel, this.docLang!, this.binPath!);
   }
 
   private initWebview(webview: vscode.Webview) {
@@ -109,39 +96,6 @@ export class CodeBlocksEditorProvider implements vscode.CustomTextEditorProvider
     `;
   }
 
-  private async updateWebview(document: vscode.TextDocument, webviewPanel: vscode.WebviewPanel) {
-    const content = document.getText();
-
-    let response: JsonResult<GetSubtreesResponse>;
-
-    try {
-      const getSubtreeArgs: GetSubtreesArgs = {
-        text: content,
-        queries: getQueryStrings(this.docLang!),
-        language: this.docLang!,
-      };
-
-      response = await codeBlocksCliClient.getSubtrees(this.binPath!, getSubtreeArgs);
-    } catch (error) {
-      vscode.window.showErrorMessage(`Failed to get blocks: ${JSON.stringify(error)}`);
-      return;
-    }
-
-    switch (response.status) {
-      case "ok":
-        webviewPanel.webview.postMessage({
-          type: "update",
-          text: content,
-          blockTrees: response.result,
-        } as UpdateMessage);
-        break;
-
-      case "error":
-        vscode.window.showErrorMessage(`Failed to get blocks: ${response.result}`);
-        break;
-    }
-  }
-
   private subscribeToDocEvents(webviewPanel: vscode.WebviewPanel, document: vscode.TextDocument) {
     const didReceiveMessageSubscription = webviewPanel.webview.onDidReceiveMessage(
       async (message: MoveCommand) => this.handleMessage(document, message),
@@ -150,7 +104,7 @@ export class CodeBlocksEditorProvider implements vscode.CustomTextEditorProvider
 
     const didChangeTextDocumentSubscription = vscode.workspace.onDidChangeTextDocument(async (e) => {
       if (e.document.uri.toString() === document.uri.toString()) {
-        await this.updateWebview(e.document, webviewPanel);
+        await updateUiBlocks(document, webviewPanel, this.docLang!, this.binPath!);
       }
     });
 
@@ -158,42 +112,5 @@ export class CodeBlocksEditorProvider implements vscode.CustomTextEditorProvider
       didChangeTextDocumentSubscription.dispose();
       didReceiveMessageSubscription.dispose();
     });
-  }
-
-  private async handleMoveCommand(
-    args: { src: BlockLocation; dst: BlockLocation },
-    document: vscode.TextDocument
-  ): Promise<void> {
-    const moveArgs: MoveBlockArgs = {
-      text: document.getText(),
-      srcBlock: args.src,
-      dstBlock: args.dst,
-      queries: getQueryStrings(this.docLang!),
-      language: this.docLang!,
-    };
-
-    let response: JsonResult<MoveBlockResponse>;
-
-    try {
-      response = await codeBlocksCliClient.moveBlock(this.binPath!, moveArgs);
-    } catch (error) {
-      vscode.window.showErrorMessage(`Failed to move block: ${JSON.stringify(error)}`);
-      return;
-    }
-
-    switch (response.status) {
-      case "ok":
-        const newContent = response.result;
-
-        const edit = new vscode.WorkspaceEdit();
-        edit.replace(document.uri, new vscode.Range(0, 0, document.lineCount, 0), newContent);
-
-        await vscode.workspace.applyEdit(edit);
-        break;
-
-      case "error":
-        vscode.window.showErrorMessage(`Failed to move block: ${response.result}`);
-        break;
-    }
   }
 }
---
+++ vscode-extension/src/uiBridge.ts
@@ -0,0 +1,89 @@
+import * as vscode from "vscode";
+import * as codeBlocksCliClient from "./codeBlocks/codeBlocksCliClient";
+import {
+  GetSubtreesArgs,
+  GetSubtreesResponse,
+  JsonResult,
+  MoveBlockArgs,
+  MoveBlockResponse,
+  SupportedLanguage,
+} from "./codeBlocks/types";
+import { getQueryStrings } from "./codeBlocks/queries";
+import { MoveCommand, UpdateMessage } from "./messages";
+
+export async function updateUiBlocks(
+  document: vscode.TextDocument,
+  webviewPanel: vscode.WebviewPanel,
+  docLang: SupportedLanguage,
+  codeBlocksCliBinPath: string
+) {
+  const content = document.getText();
+
+  let response: JsonResult<GetSubtreesResponse>;
+
+  try {
+    const getSubtreeArgs: GetSubtreesArgs = {
+      text: content,
+      queries: getQueryStrings(docLang),
+      language: docLang,
+    };
+
+    response = await codeBlocksCliClient.getSubtrees(codeBlocksCliBinPath, getSubtreeArgs);
+  } catch (error) {
+    vscode.window.showErrorMessage(`Failed to get blocks: ${JSON.stringify(error)}`);
+    return;
+  }
+
+  switch (response.status) {
+    case "ok":
+      webviewPanel.webview.postMessage({
+        type: "update",
+        text: content,
+        blockTrees: response.result,
+      } as UpdateMessage);
+      break;
+
+    case "error":
+      vscode.window.showErrorMessage(`Failed to get blocks: ${response.result}`);
+      break;
+  }
+}
+
+export async function moveBlock(
+  msg: MoveCommand,
+  document: vscode.TextDocument,
+  docLang: SupportedLanguage,
+  codeBlocksCliBinPath: string
+): Promise<void> {
+  const moveArgs: MoveBlockArgs = {
+    text: document.getText(),
+    srcBlock: msg.args.src,
+    dstBlock: msg.args.dst,
+    queries: getQueryStrings(docLang),
+    language: docLang,
+  };
+
+  let response: JsonResult<MoveBlockResponse>;
+
+  try {
+    response = await codeBlocksCliClient.moveBlock(codeBlocksCliBinPath, moveArgs);
+  } catch (error) {
+    vscode.window.showErrorMessage(`Failed to move block: ${JSON.stringify(error)}`);
+    return;
+  }
+
+  switch (response.status) {
+    case "ok":
+      const newContent = response.result;
+
+      const edit = new vscode.WorkspaceEdit();
+      edit.replace(document.uri, new vscode.Range(0, 0, document.lineCount, 0), newContent);
+
+      await vscode.workspace.applyEdit(edit);
+      break;
+
+    case "error":
+      vscode.window.showErrorMessage(`Failed to move block: ${response.result}`);
+      break;
+  }
+}
