a538b79e1d413590727edb2c3cacc15c91ca1331
Tom Selfin <selfint@gmail.com>
2023-02-27 21:46:52 +0200

    add build_query method to DynamicParser

--- code-blocks-server/tree-sitter-installer/src/dynamic_parser.rs
+++ code-blocks-server/tree-sitter-installer/src/dynamic_parser.rs
@@ -1,7 +1,7 @@
 use libloading::Library;
 
-use anyhow::Result;
-use tree_sitter::{Language, Parser, Tree};
+use anyhow::{Context, Result};
+use tree_sitter::{Language, Parser, Query, Tree};
 
 /// A [tree_sitter::Parser] object using a [tree_sitter::Language] object
 /// loaded at runtime using [libloading].
@@ -16,22 +16,31 @@ use tree_sitter::{Language, Parser, Tree};
 ///
 /// ## Drop
 /// When dropping this struct, the Library need to be dropped last. That is why the order
-/// of the fields is parser, lib. Changing this order will result in a segfault.
+/// of the fields is parser, language, lib. Changing this order will result in a segfault.
 pub struct DynamicParser {
     parser: Parser,
+    language: Language,
     _lib: Library,
 }
 
 impl DynamicParser {
+    pub fn new(lib: Library, lang: Language) -> Result<Self> {
+        let mut parser = Parser::new();
+        parser.set_language(lang)?;
+
+        Ok(Self {
+            _lib: lib,
+            language: lang,
+            parser,
+        })
+    }
+
     /// Wrapper method around [tree_sitter::Parser]'s parse method.
     pub fn parse(&mut self, text: impl AsRef<[u8]>, old_tree: Option<&Tree>) -> Option<Tree> {
         self.parser.parse(text, old_tree)
     }
 
-    pub fn new(lib: Library, lang: Language) -> Result<Self> {
-        let mut parser = Parser::new();
-        parser.set_language(lang)?;
-
-        Ok(Self { _lib: lib, parser })
+    pub fn build_query(&self, source: &str) -> Result<Query> {
+        Query::new(self.language, source).context("failed to build query")
     }
 }
