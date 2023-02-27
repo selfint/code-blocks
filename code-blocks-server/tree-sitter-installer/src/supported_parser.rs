632a215ca8028eee4b38332fb68aaa72746d318d
Tom Selfin <selfint@gmail.com>
2023-02-27 21:46:28 +0200

    make ParserInstaller contain owned fields

--- code-blocks-server/tree-sitter-installer/src/supported_parser.rs
+++ code-blocks-server/tree-sitter-installer/src/supported_parser.rs
@@ -12,29 +12,34 @@ impl SupportedParser {
     pub fn get_installer(&self) -> ParserInstaller {
         match self {
             Self::Rust => ParserInstaller {
-                download_cmd: "git clone https://github.com/tree-sitter/tree-sitter-rust",
-                symbol: b"language",
-                name: "tree_sitter_rust",
+                download_cmd: "git clone https://github.com/tree-sitter/tree-sitter-rust"
+                    .to_string(),
+                symbol: b"language".to_vec(),
+                name: "tree_sitter_rust".to_string(),
             },
             Self::TypeScript => ParserInstaller {
-                download_cmd: "git clone https://github.com/tree-sitter/tree-sitter-typescript",
-                symbol: b"language_typescript",
-                name: "tree_sitter_typescript",
+                download_cmd: "git clone https://github.com/tree-sitter/tree-sitter-typescript"
+                    .to_string(),
+                symbol: b"language_typescript".to_vec(),
+                name: "tree_sitter_typescript".to_string(),
             },
             Self::TSX => ParserInstaller {
-                download_cmd: "git clone https://github.com/tree-sitter/tree-sitter-typescript",
-                symbol: b"language_tsx",
-                name: "tree_sitter_typescript",
+                download_cmd: "git clone https://github.com/tree-sitter/tree-sitter-typescript"
+                    .to_string(),
+                symbol: b"language_tsx".to_vec(),
+                name: "tree_sitter_typescript".to_string(),
             },
             Self::Svelte => ParserInstaller {
-                download_cmd: "git clone https://github.com/Himujjal/tree-sitter-svelte",
-                symbol: b"language",
-                name: "tree_sitter_svelte",
+                download_cmd: "git clone https://github.com/Himujjal/tree-sitter-svelte"
+                    .to_string(),
+                symbol: b"language".to_vec(),
+                name: "tree_sitter_svelte".to_string(),
             },
             Self::Python => ParserInstaller {
-                download_cmd: "git clone https://github.com/tree-sitter/tree-sitter-python",
-                symbol: b"language",
-                name: "tree_sitter_python",
+                download_cmd: "git clone https://github.com/tree-sitter/tree-sitter-python"
+                    .to_string(),
+                symbol: b"language".to_vec(),
+                name: "tree_sitter_python".to_string(),
             },
         }
     }
