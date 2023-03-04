4f9289e0f8ce978f6e69ed01ecbd91e9bede0687
selfint <40059156+selfint@users.noreply.github.com>
2023-03-04 13:31:11 +0200

    54-improve-grammar-installation-experience (#55)

--- code-blocks-server/tree-sitter-installer/tests/test_tree_sitter_installer.rs
+++ code-blocks-server/tree-sitter-installer/tests/test_tree_sitter_installer.rs
@@ -1,30 +1,65 @@
-use tree_sitter_installer::*;
+use tree_sitter_installer::{
+    parser_installer::{self, InstallationStatus},
+    DynamicParser,
+};
 
 #[test]
 fn test_install_and_load_parser() {
-    let target_dir = tempfile::tempdir()
+    let download_cmd = "git clone https://github.com/tree-sitter/tree-sitter-rust";
+    let language_fn_symbol = b"language";
+    let library_name = "tree_sitter_rust";
+    let install_dir = tempfile::tempdir()
         .expect("failed to get tempdir")
-        .into_path()
-        .join("test_install_lang")
-        .join("test-parser");
+        .path()
+        .join("rust-parser");
 
-    let installer = SupportedParser::Rust.get_installer();
+    let mut reported_progresses = vec![];
+    let report_progress = |status: InstallationStatus| match status {
+        InstallationStatus::Downloading(_) => reported_progresses.push("Downloading"),
+        InstallationStatus::Patching => reported_progresses.push("Patching"),
+        InstallationStatus::Compiling(_) => reported_progresses.push("Compiling"),
+    };
 
-    let mut parser = installer
-        .install_parser(&target_dir)
-        .expect("failed to install lang");
+    let library_path = parser_installer::install_parser(
+        download_cmd,
+        library_name,
+        &install_dir,
+        Some(report_progress),
+    )
+    .expect("failed to install rust parser");
+
+    reported_progresses.sort();
+
+    let mut parser = DynamicParser::load_from(&library_path, language_fn_symbol)
+        .expect("failed to install rust parser");
 
     let src = "fn main() {}";
 
     let tree = parser.parse(src, None);
 
-    insta::assert_snapshot!(tree.unwrap().root_node().to_sexp(), @"(source_file (function_item name: (identifier) parameters: (parameters) body: (block)))");
+    insta::assert_snapshot!(tree.unwrap().root_node().to_sexp(),
+        @"(source_file (function_item name: (identifier) parameters: (parameters) body: (block)))"
+    );
 
-    let mut parser = installer
-        .load_parser(&target_dir)
-        .expect("failed to load dynamic language");
-
-    let tree = parser.parse(src, None);
-
-    insta::assert_snapshot!(tree.unwrap().root_node().to_sexp(), @"(source_file (function_item name: (identifier) parameters: (parameters) body: (block)))");
+    insta::assert_debug_snapshot!(reported_progresses,
+        @r###"
+    [
+        "Compiling",
+        "Compiling",
+        "Compiling",
+        "Compiling",
+        "Compiling",
+        "Compiling",
+        "Compiling",
+        "Compiling",
+        "Compiling",
+        "Compiling",
+        "Downloading",
+        "Downloading",
+        "Downloading",
+        "Downloading",
+        "Patching",
+    ]
+    "###
+    )
 }
