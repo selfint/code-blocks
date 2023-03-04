3f86946f258d1933b99e8c608f7b901c586c5a31
selfint <selfint@gmail.com>
2023-03-04 13:08:21 +0200

    installer: fix progress reporting

--- code-blocks-server/tree-sitter-installer/src/parser_installer.rs
+++ code-blocks-server/tree-sitter-installer/src/parser_installer.rs
@@ -1,8 +1,9 @@
-use anyhow::{bail, Context, Result};
+use anyhow::{anyhow, Context, Result};
 use std::{
     fs::read,
+    io::{BufRead, BufReader},
     path::{Path, PathBuf},
-    process::{Command, ExitStatus},
+    process::{Command, Output},
     str::from_utf8,
 };
 
@@ -23,9 +24,9 @@ pub fn is_installed_at(library_name: &str, install_dir: &Path) -> bool {
 
 #[derive(Debug)]
 pub enum InstallationStatus {
-    Downloading,
+    Downloading(String),
     Patching,
-    Compiling,
+    Compiling(String),
 }
 
 pub fn install_parser(
@@ -35,40 +36,107 @@ pub fn install_parser(
     mut report_progress: Option<impl FnMut(InstallationStatus)>,
 ) -> Result<PathBuf> {
     if let Some(report_progress) = &mut report_progress {
-        report_progress(InstallationStatus::Downloading)
+        report_progress(InstallationStatus::Downloading("Start".to_string()));
     }
-    if !download_parser(download_cmd, install_dir)? {
-        bail!("failed to download parser")
+    download_parser(download_cmd, install_dir, &mut report_progress)?;
+    if let Some(report_progress) = &mut report_progress {
+        report_progress(InstallationStatus::Downloading("Done".to_string()));
     }
 
     if let Some(report_progress) = &mut report_progress {
-        report_progress(InstallationStatus::Patching)
+        report_progress(InstallationStatus::Patching);
     }
     disable_language_fn_mangle(install_dir).context("failed to disable language fn mangle")?;
 
     if let Some(report_progress) = &mut report_progress {
-        report_progress(InstallationStatus::Compiling)
+        report_progress(InstallationStatus::Compiling("Start".to_string()));
     }
-    compile_parser(install_dir).context("failed to build test parser")?;
+    compile_parser(install_dir, &mut report_progress)?;
 
     Ok(get_compiled_lib_path(library_name, install_dir))
 }
 
-fn download_parser(download_cmd: &str, target_dir: &Path) -> Result<bool> {
+fn _run_cmd_with_progress(
+    cmd: &mut Command,
+    report_progress: &mut Option<impl FnMut(String)>,
+) -> Result<Output> {
+    let mut child = cmd
+        .stdout(std::process::Stdio::piped())
+        .stderr(std::process::Stdio::piped())
+        .spawn()
+        .context("failed to start cmd")?;
+
+    let stdout = child.stdout.take().context("failed to read stdout")?;
+    let stderr = child.stderr.take().context("failed to read stdout")?;
+
+    let mut out_lines = BufReader::new(stdout).lines();
+    let mut error_lines = BufReader::new(stderr).lines();
+
+    while child.try_wait()?.is_none() {
+        while let Some(Ok(line)) = out_lines.next() {
+            if let Some(report_progress) = report_progress {
+                report_progress(line);
+            }
+        }
+        while let Some(Ok(line)) = error_lines.next() {
+            if let Some(report_progress) = report_progress {
+                report_progress(line);
+            }
+        }
+    }
+
+    // final lines
+    while let Some(Ok(line)) = out_lines.next() {
+        if let Some(report_progress) = report_progress {
+            report_progress(line);
+        }
+    }
+    while let Some(Ok(line)) = error_lines.next() {
+        if let Some(report_progress) = report_progress {
+            report_progress(line);
+        }
+    }
+
+    child.wait_with_output().context("failed to run cmd")
+}
+
+fn download_parser(
+    download_cmd: &str,
+    target_dir: &Path,
+    report_progress: &mut Option<impl FnMut(InstallationStatus)>,
+) -> Result<()> {
     let cmd = download_cmd
         .split_ascii_whitespace()
         .next()
         .context("got empty download command")?;
     let args: Vec<_> = download_cmd.split_ascii_whitespace().skip(1).collect();
 
-    Ok(Command::new(cmd)
-        .args(args)
-        .arg(target_dir)
-        .spawn()
-        .context("failed to start download cmd")?
-        .wait()
-        .context("failed to run download cmd")?
-        .success())
+    if let Some(report_progress) = report_progress {
+        report_progress(InstallationStatus::Downloading(format!(
+            "Running command '{} {}'",
+            download_cmd,
+            target_dir.to_string_lossy()
+        )))
+    }
+
+    let mut outputs = vec![];
+    let mut report_progress = report_progress.as_mut().map(|report_progress| {
+        |line: String| {
+            outputs.push(line.clone());
+            report_progress(InstallationStatus::Downloading(line));
+        }
+    });
+
+    let output = _run_cmd_with_progress(
+        Command::new(cmd).args(args).arg(target_dir),
+        &mut report_progress,
+    )?;
+
+    if !output.status.success() {
+        Err(anyhow!("failed to download parser: {}", outputs.join("\n")))
+    } else {
+        Ok(())
+    }
 }
 
 fn disable_language_fn_mangle(parser_dir: &Path) -> Result<()> {
@@ -91,15 +159,29 @@ fn disable_language_fn_mangle(parser_dir: &Path) -> Result<()> {
     std::fs::write(lib_file, new_lib_file_src).context("failed to write new lib file src")
 }
 
-fn compile_parser(parser_dir: &Path) -> Result<ExitStatus> {
+fn compile_parser(
+    parser_dir: &Path,
+    report_progress: &mut Option<impl FnMut(InstallationStatus)>,
+) -> Result<()> {
     let cmd = BUILD_CMD.split_ascii_whitespace().next().unwrap();
     let args: Vec<_> = BUILD_CMD.split_ascii_whitespace().skip(1).collect();
 
-    Command::new(cmd)
-        .args(args)
-        .current_dir(parser_dir)
-        .spawn()
-        .context("failed to build parser")?
-        .wait()
-        .context("failed to run build cmd")
+    let mut outputs = vec![];
+    let mut report_progress = report_progress.as_mut().map(|report_progress| {
+        |line: String| {
+            outputs.push(line.clone());
+            report_progress(InstallationStatus::Compiling(line));
+        }
+    });
+
+    let output = _run_cmd_with_progress(
+        Command::new(cmd).args(args).current_dir(parser_dir),
+        &mut report_progress,
+    )?;
+
+    if !output.status.success() {
+        Err(anyhow!("failed to compile parser: {}", outputs.join("\n")))
+    } else {
+        Ok(())
+    }
 }
--- code-blocks-server/tree-sitter-installer/tests/test_tree_sitter_installer.rs
+++ code-blocks-server/tree-sitter-installer/tests/test_tree_sitter_installer.rs
@@ -15,9 +15,27 @@ fn test_install_and_load_parser() {
 
     let mut reported_progresses = vec![];
     let report_progress = |status: InstallationStatus| match status {
-        InstallationStatus::Downloading => reported_progresses.push("Downloading"),
-        InstallationStatus::Patching => reported_progresses.push("Patching"),
-        InstallationStatus::Compiling => reported_progresses.push("Compiling"),
+        InstallationStatus::Downloading(string) => reported_progresses.push(format!(
+            "Downloading: {}",
+            string
+                .trim()
+                .split_ascii_whitespace()
+                .take(2)
+                .map(|w| format!("{} ", w))
+                .collect::<String>()
+                .trim()
+        )),
+        InstallationStatus::Patching => reported_progresses.push("Patching".to_string()),
+        InstallationStatus::Compiling(string) => reported_progresses.push(format!(
+            "Compiling: {}",
+            string
+                .trim()
+                .split_ascii_whitespace()
+                .take(2)
+                .map(|w| format!("{} ", w))
+                .collect::<String>()
+                .trim()
+        )),
     };
 
     let library_path = parser_installer::install_parser(
@@ -27,6 +45,8 @@ fn test_install_and_load_parser() {
         Some(report_progress),
     )
     .expect("failed to install rust parser");
+
+    reported_progresses.sort();
 
     let mut parser = DynamicParser::load_from(&library_path, language_fn_symbol)
         .expect("failed to install rust parser");
@@ -42,9 +62,21 @@ fn test_install_and_load_parser() {
     insta::assert_debug_snapshot!(reported_progresses,
         @r###"
     [
-        "Downloading",
+        "Compiling: Compiling aho-corasick",
+        "Compiling: Compiling cc",
+        "Compiling: Compiling memchr",
+        "Compiling: Compiling regex",
+        "Compiling: Compiling regex-syntax",
+        "Compiling: Compiling tree-sitter",
+        "Compiling: Compiling tree-sitter-rust",
+        "Compiling: Finished release",
+        "Compiling: Start",
+        "Compiling: Updating crates.io",
+        "Downloading: Cloning into",
+        "Downloading: Done",
+        "Downloading: Running command",
+        "Downloading: Start",
         "Patching",
-        "Compiling",
     ]
     "###
     )
