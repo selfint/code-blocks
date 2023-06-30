b5bbeb11a452b19665feb5a24876f716df210af8
selfint <selfint@gmail.com>
2023-06-30 16:00:38 +0300

    add tsx tests to FielTree tests

--- src/test/suite/codeBlocks/FileTree.test.ts
+++ src/test/suite/codeBlocks/FileTree.test.ts
@@ -8,105 +8,212 @@ import { expect } from "chai";
 const parsersDir = "/Users/selfint/dev/github.com/selfint/code-blocks/parsers";
 
 suite("FileTree", function () {
-    // @ts-expect-error initialized in beforeAll
-    let language: Language = undefined;
-    this.beforeAll(async () => {
-        const lang = await Installer.loadParser(parsersDir, "tree-sitter-rust");
-        assert.ok(lang);
-        language = lang;
-    });
-
-    async function buildFileTree(content: string): Promise<FileTree> {
-        const document = await vscode.workspace.openTextDocument({
-            language: "rust",
-            content,
+    suite("Rust", function () {
+        // @ts-expect-error initialized in beforeAll
+        let language: Language = undefined;
+        this.beforeAll(async () => {
+            const lang = await Installer.loadParser(parsersDir, "tree-sitter-rust");
+            assert.ok(lang);
+            language = lang;
         });
-        const fileTree = await FileTree.new(language, document);
-        return fileTree;
-    }
 
-    suite(".update", function () {
-        test("updates tree after edit", async () => {
-            const content = "fn main() {}";
-            const fileTree = await buildFileTree(content);
-            expect("\n" + fileTree.toString()).to.equal(`
+        async function buildFileTree(content: string): Promise<FileTree> {
+            const document = await vscode.workspace.openTextDocument({
+                language: "rust",
+                content,
+            });
+            const fileTree = await FileTree.new(language, document);
+            return fileTree;
+        }
+
+        suite(".update", function () {
+            test("updates tree after edit", async () => {
+                const content = "fn main() {}";
+                const fileTree = await buildFileTree(content);
+                expect("\n" + fileTree.toString()).to.equal(`
 source_file [0:0 - 0:12]
   function_item [0:0 - 0:12]
     identifier [0:3 - 0:7]
     parameters [0:7 - 0:9]
     block [0:10 - 0:12]`);
 
-            const edit = new vscode.WorkspaceEdit();
-            edit.replace(
-                fileTree.document.uri,
-                new vscode.Range(new vscode.Position(0, 0), new vscode.Position(0, content.length)),
-                "struct A;"
-            );
+                const edit = new vscode.WorkspaceEdit();
+                edit.replace(
+                    fileTree.document.uri,
+                    new vscode.Range(new vscode.Position(0, 0), new vscode.Position(0, content.length)),
+                    "struct A;"
+                );
 
-            await vscode.workspace.applyEdit(edit);
+                await vscode.workspace.applyEdit(edit);
 
-            expect("\n" + fileTree.toString()).to.equal(`
+                expect("\n" + fileTree.toString()).to.equal(`
 source_file [0:0 - 0:9]
   struct_item [0:0 - 0:9]
     type_identifier [0:7 - 0:8]`);
+            });
+        });
+
+        suite(".resolveVscodeSelection", () => {
+            async function testResolveVscodeSelection(
+                content: string,
+                selectionRange: [number, number, number, number],
+                expectedSelectionText: string | undefined
+            ): Promise<void> {
+                const fileTree = await buildFileTree(content);
+                const selection = fileTree.resolveVscodeSelection(
+                    new vscode.Selection(
+                        new vscode.Position(selectionRange[0], selectionRange[1]),
+                        new vscode.Position(selectionRange[2], selectionRange[3])
+                    )
+                );
+
+                expect(selection?.getText(content)).to.equal(expectedSelectionText);
+            }
+
+            test("is node when selection is node range", async () => {
+                await testResolveVscodeSelection(
+                    "fn main() { let a = 1; let b = 2; }",
+                    [0, 12, 0, 22],
+                    "let a = 1;"
+                );
+            });
+
+            test("expands to node when range within node range", async () => {
+                await testResolveVscodeSelection(
+                    "fn main() { let a = 1; let b = 2; }",
+                    [0, 18, 0, 20],
+                    "let a = 1;"
+                );
+            });
+
+            test("is multiple nodes when range is multiple nodes range", async () => {
+                await testResolveVscodeSelection(
+                    "fn main() { let a = 1; let b = 2; }",
+                    [0, 12, 0, 33],
+                    "let a = 1; let b = 2;"
+                );
+            });
+
+            test("is multiple nodes when range within multiple nodes ranges", async () => {
+                await testResolveVscodeSelection(
+                    "fn main() { let a = 1; let b = 2; }",
+                    [0, 15, 0, 24],
+                    "let a = 1; let b = 2;"
+                );
+            });
+
+            test("expands scope when selection crosses node parent range", async () => {
+                await testResolveVscodeSelection(
+                    "fn main() { fn foo() { let a = 1; } let b = 2; }",
+                    [0, 26, 0, 38],
+                    "fn foo() { let a = 1; } let b = 2;"
+                );
+            });
         });
     });
 
-    suite(".resolveVscodeSelection", () => {
-        async function testResolveVscodeSelection(
-            content: string,
-            selectionRange: [number, number, number, number],
-            expectedSelectionText: string | undefined
-        ): Promise<void> {
-            const fileTree = await buildFileTree(content);
-            const selection = fileTree.resolveVscodeSelection(
-                new vscode.Selection(
-                    new vscode.Position(selectionRange[0], selectionRange[1]),
-                    new vscode.Position(selectionRange[2], selectionRange[3])
-                )
-            );
+    suite("TSX", function () {
+        // @ts-expect-error initialized in beforeAll
+        let language: Language = undefined;
+        this.beforeAll(async () => {
+            const lang = await Installer.loadParser(parsersDir, "tree-sitter-typescript", "tree-sitter-tsx");
+            assert.ok(lang);
+            language = lang;
+        });
 
-            expect(selection?.getText(content)).to.equal(expectedSelectionText);
+        async function buildFileTree(content: string): Promise<FileTree> {
+            const document = await vscode.workspace.openTextDocument({
+                language: "typescriptreact",
+                content,
+            });
+            const fileTree = await FileTree.new(language, document);
+            return fileTree;
         }
 
-        test("is node when selection is node range", async () => {
-            await testResolveVscodeSelection(
-                "fn main() { let a = 1; let b = 2; }",
-                [0, 12, 0, 22],
-                "let a = 1;"
-            );
+        suite(".update", function () {
+            test("updates tree after edit", async () => {
+                const content = "function main() {}";
+                const fileTree = await buildFileTree(content);
+                expect("\n" + fileTree.toString()).to.equal(`
+program [0:0 - 0:18]
+  function_declaration [0:0 - 0:18]
+    identifier [0:9 - 0:13]
+    formal_parameters [0:13 - 0:15]
+    statement_block [0:16 - 0:18]`);
+
+                const edit = new vscode.WorkspaceEdit();
+                edit.replace(
+                    fileTree.document.uri,
+                    new vscode.Range(new vscode.Position(0, 0), new vscode.Position(0, content.length)),
+                    "class A {}"
+                );
+
+                await vscode.workspace.applyEdit(edit);
+
+                expect("\n" + fileTree.toString()).to.equal(`
+program [0:0 - 0:10]
+  class_declaration [0:0 - 0:10]
+    type_identifier [0:6 - 0:7]
+    class_body [0:8 - 0:10]`);
+            });
         });
 
-        test("is node when range within node range", async () => {
-            await testResolveVscodeSelection(
-                "fn main() { let a = 1; let b = 2; }",
-                [0, 18, 0, 20],
-                "let a = 1;"
-            );
-        });
+        suite(".resolveVscodeSelection", () => {
+            async function testResolveVscodeSelection(
+                content: string,
+                selectionRange: [number, number, number, number],
+                expectedSelectionText: string | undefined
+            ): Promise<void> {
+                const fileTree = await buildFileTree(content);
+                const selection = fileTree.resolveVscodeSelection(
+                    new vscode.Selection(
+                        new vscode.Position(selectionRange[0], selectionRange[1]),
+                        new vscode.Position(selectionRange[2], selectionRange[3])
+                    )
+                );
 
-        test("is multiple nodes when range is multiple nodes range", async () => {
-            await testResolveVscodeSelection(
-                "fn main() { let a = 1; let b = 2; }",
-                [0, 12, 0, 33],
-                "let a = 1; let b = 2;"
-            );
-        });
+                expect(selection?.getText(content)).to.equal(expectedSelectionText);
+            }
 
-        test("is multiple nodes when range within multiple nodes ranges", async () => {
-            await testResolveVscodeSelection(
-                "fn main() { let a = 1; let b = 2; }",
-                [0, 15, 0, 24],
-                "let a = 1; let b = 2;"
-            );
-        });
+            test("is node when selection is node range", async () => {
+                await testResolveVscodeSelection(
+                    "function main() { return (<> <p>a</p> <p>b</p> </>); }",
+                    [0, 32, 0, 33],
+                    "a"
+                );
+            });
 
-        test("expands scope when selection crosses node parent range", async () => {
-            await testResolveVscodeSelection(
-                "fn main() { fn foo() { let a = 1; } let b = 2; }",
-                [0, 26, 0, 38],
-                "fn foo() { let a = 1; } let b = 2;"
-            );
+            test("expands to node when range within node range", async () => {
+                await testResolveVscodeSelection(
+                    "function main() { return (<> <p>a</p> <p>b</p> </>); }",
+                    [0, 29, 0, 31],
+                    "<p>"
+                );
+            });
+
+            test("is multiple nodes when range is multiple nodes range", async () => {
+                await testResolveVscodeSelection(
+                    "function main() { return (<> <p>a</p> <p>b</p> </>); }",
+                    [0, 29, 0, 37],
+                    "<p>a</p>"
+                );
+            });
+
+            test("is multiple nodes when range within multiple nodes ranges", async () => {
+                await testResolveVscodeSelection(
+                    "function main() { return (<> <p>a</p> <p>b</p> </>); }",
+                    [0, 31, 0, 35],
+                    "<p>a</p>"
+                );
+            });
+
+            test("expands scope when selection crosses node parent range", async () => {
+                await testResolveVscodeSelection(
+                    "function main() { return (<> <p>a</p> <p>b</p> </>); }",
+                    [0, 33, 0, 42],
+                    "<p>a</p> <p>b</p>"
+                );
+            });
         });
     });
 });
