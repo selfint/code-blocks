22575231451b44fdfe9b0925db7581be2b4f6cc3
selfint <selfint@gmail.com>
2023-06-30 12:53:07 +0300

    add FileTree test

---
+++ src/test/suite/codeBlocks/FileTree.test.ts
@@ -0,0 +1,45 @@
+import * as Installer from "../../../Installer";
+import * as vscode from "vscode";
+import { FileTree } from "../../../FileTree";
+import { Language } from "web-tree-sitter";
+import assert from "assert";
+import { expect } from "chai";
+
+const parsersDir = "parsers";
+
+suite("FileTree", function () {
+    // @ts-expect-error initialized in beforeAll
+    let language: Language = undefined;
+    this.beforeAll(async () => {
+        const lang = await Installer.loadParser(parsersDir, "tree-sitter-rust");
+        assert.ok(lang);
+        language = lang;
+    });
+
+    suite(".update", function () {
+        test("updates tree after edit", async () => {
+            const content = "fn main() {}";
+            const document = await vscode.workspace.openTextDocument({
+                language: "rust",
+                content,
+            });
+            const fileTree = await FileTree.new(language, document);
+            expect(fileTree.tree.rootNode.toString()).to.equal(
+                "(source_file (function_item name: (identifier) parameters: (parameters) body: (block)))"
+            );
+
+            const edit = new vscode.WorkspaceEdit();
+            edit.replace(
+                document.uri,
+                new vscode.Range(new vscode.Position(0, 0), new vscode.Position(0, content.length)),
+                "struct A;"
+            );
+
+            await vscode.workspace.applyEdit(edit);
+
+            expect(fileTree.tree.rootNode.toString()).to.equal(
+                "(source_file (struct_item name: (type_identifier)))"
+            );
+        });
+    });
+});
