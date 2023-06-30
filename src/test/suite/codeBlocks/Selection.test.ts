118cd7398701e8e724372d0c687dab1c285f43bf
selfint <selfint@gmail.com>
2023-06-30 12:08:49 +0300

    organize tests

--- src/test/suite/codeBlocks/Selection.test.ts
+++ src/test/suite/codeBlocks/Selection.test.ts
@@ -49,73 +49,83 @@ function buildUpdateRunner(cursor: string, language: [string, Language]): Update
 }
 
 suite("Selection", function () {
-    suite("Rust", function () {
-        const cursor = "@";
+    suite(".update", function () {
+        suite("Rust", function () {
+            const cursor = "@";
 
-        // @ts-expect-error initialized in beforeAll
-        let selectionAt: UpdateRunner = undefined;
-        this.beforeAll(async () => {
-            const rust = await Installer.loadParser(parsersDir, "tree-sitter-rust");
-            assert.ok(rust);
-            return (selectionAt = buildUpdateRunner(cursor, ["rust", rust]));
+            // @ts-expect-error initialized in beforeAll
+            let selectionAt: UpdateRunner = undefined;
+            this.beforeAll(async () => {
+                const rust = await Installer.loadParser(parsersDir, "tree-sitter-rust");
+                assert.ok(rust);
+                return (selectionAt = buildUpdateRunner(cursor, ["rust", rust]));
+            });
+
+            test("Select source_file node is undefined", async () => {
+                expect(await selectionAt("fn main() { }@")).to.be.undefined;
+            });
+
+            test("Update selection parent/child", async () => {
+                expect(await selectionAt("fn main() { @ }")).to.equal("{  }");
+                expect(await selectionAt("fn main() { @ }", ["parent"])).to.equal("fn main() {  }");
+                expect(await selectionAt("fn main() { @ }", ["parent", "child"])).to.equal("{  }");
+            });
+
+            test("Update selection previous/next", async () => {
+                const text = "[1, @2, 3]";
+
+                expect(await selectionAt(text)).to.equal("2");
+                expect(await selectionAt(text, ["add-previous"])).to.equal("1, 2");
+                expect(await selectionAt(text, ["add-next"])).to.equal("2, 3");
+                expect(await selectionAt(text, ["add-previous", "remove-next"])).to.equal("1");
+                expect(await selectionAt(text, ["add-previous", "remove-previous"])).to.equal("2");
+                expect(await selectionAt(text, ["add-next", "remove-next"])).to.equal("2");
+                expect(await selectionAt(text, ["add-next", "remove-previous"])).to.equal("3");
+            });
         });
 
-        test("Select source_file node is undefined", async () => {
-            expect(await selectionAt("fn main() { }@")).to.be.undefined;
-        });
+        suite("TSX", function () {
+            const cursor = "@";
+            // @ts-expect-error initialized in beforeAll
+            let selectionAt: UpdateRunner = undefined;
+            this.beforeAll(async () => {
+                const tsx = await Installer.loadParser(
+                    parsersDir,
+                    "tree-sitter-typescript",
+                    "tree-sitter-tsx"
+                );
+                assert.ok(tsx);
+                selectionAt = buildUpdateRunner(cursor, ["typescriptreact", tsx]);
+            });
 
-        test("Update selection parent/child", async () => {
-            expect(await selectionAt("fn main() { @ }")).to.equal("{  }");
-            expect(await selectionAt("fn main() { @ }", ["parent"])).to.equal("fn main() {  }");
-            expect(await selectionAt("fn main() { @ }", ["parent", "child"])).to.equal("{  }");
-        });
+            test("Select source_file node is undefined", async () => {
+                expect(await selectionAt("function main() { }@")).to.be.undefined;
+            });
 
-        test("Update selection previous/next", async () => {
-            const text = "[1, @2, 3]";
+            test("Update selection parent/child", async () => {
+                const text = "function main() { @ }";
+                expect(await selectionAt(text)).to.equal("{  }");
+                expect(await selectionAt(text, ["parent"])).to.equal("function main() {  }");
+                expect(await selectionAt(text, ["parent", "child"])).to.equal("{  }");
+            });
 
-            expect(await selectionAt(text)).to.equal("2");
-            expect(await selectionAt(text, ["add-previous"])).to.equal("1, 2");
-            expect(await selectionAt(text, ["add-next"])).to.equal("2, 3");
-            expect(await selectionAt(text, ["add-previous", "remove-next"])).to.equal("1");
-            expect(await selectionAt(text, ["add-previous", "remove-previous"])).to.equal("2");
-            expect(await selectionAt(text, ["add-next", "remove-next"])).to.equal("2");
-            expect(await selectionAt(text, ["add-next", "remove-previous"])).to.equal("3");
-        });
-    });
+            test("Update selection previous/next", async () => {
+                const text = "(<ul><li>1</li><li>@2</li><li>3</li></ul>)";
 
-    suite("TSX", function () {
-        const cursor = "@";
-        // @ts-expect-error initialized in beforeAll
-        let selectionAt: UpdateRunner = undefined;
-        this.beforeAll(async () => {
-            const tsx = await Installer.loadParser(parsersDir, "tree-sitter-typescript", "tree-sitter-tsx");
-            assert.ok(tsx);
-            selectionAt = buildUpdateRunner(cursor, ["typescriptreact", tsx]);
-        });
-
-        test("Select source_file node is undefined", async () => {
-            expect(await selectionAt("function main() { }@")).to.be.undefined;
-        });
-
-        test("Update selection parent/child", async () => {
-            const text = "function main() { @ }";
-            expect(await selectionAt(text)).to.equal("{  }");
-            expect(await selectionAt(text, ["parent"])).to.equal("function main() {  }");
-            expect(await selectionAt(text, ["parent", "child"])).to.equal("{  }");
-        });
-
-        test("Update selection previous/next", async () => {
-            const text = "(<ul><li>1</li><li>@2</li><li>3</li></ul>)";
-
-            expect(await selectionAt(text)).to.equal("2");
-            expect(await selectionAt(text, ["parent", "add-previous"])).to.equal("<li>1</li><li>2</li>");
-            expect(await selectionAt(text, ["parent", "add-next"])).to.equal("<li>2</li><li>3</li>");
-            expect(await selectionAt(text, ["parent", "add-previous", "remove-next"])).to.equal("<li>1</li>");
-            expect(await selectionAt(text, ["parent", "add-previous", "remove-previous"])).to.equal(
-                "<li>2</li>"
-            );
-            expect(await selectionAt(text, ["parent", "add-next", "remove-next"])).to.equal("<li>2</li>");
-            expect(await selectionAt(text, ["parent", "add-next", "remove-previous"])).to.equal("<li>3</li>");
+                expect(await selectionAt(text)).to.equal("2");
+                expect(await selectionAt(text, ["parent", "add-previous"])).to.equal("<li>1</li><li>2</li>");
+                expect(await selectionAt(text, ["parent", "add-next"])).to.equal("<li>2</li><li>3</li>");
+                expect(await selectionAt(text, ["parent", "add-previous", "remove-next"])).to.equal(
+                    "<li>1</li>"
+                );
+                expect(await selectionAt(text, ["parent", "add-previous", "remove-previous"])).to.equal(
+                    "<li>2</li>"
+                );
+                expect(await selectionAt(text, ["parent", "add-next", "remove-next"])).to.equal("<li>2</li>");
+                expect(await selectionAt(text, ["parent", "add-next", "remove-previous"])).to.equal(
+                    "<li>3</li>"
+                );
+            });
         });
     });
 });
