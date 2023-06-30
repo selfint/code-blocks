1f34a8ef1723fd086712a0129e50343587b31433
selfint <selfint@gmail.com>
2023-06-30 18:58:03 +0300

    add tree viewer test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,6 +1,7 @@
 import * as vscode from "vscode";
 import { CodeBlocksEditorProvider } from "../../editor/CodeBlocksEditorProvider";
-import { assert } from "chai";
+import { assert, expect } from "chai";
+import { TreeViewer } from "../../TreeViewer";
 
 async function openDocument(content: string, language: string): Promise<void> {
     await vscode.window.showTextDocument(
@@ -26,6 +27,22 @@ suite("codeBlocks commands", function () {
                 (vscode.window.tabGroups.activeTabGroup.activeTab?.input as { viewType: string }).viewType,
                 CodeBlocksEditorProvider.viewType
             );
+        });
+    });
+
+    suite(".openTreeViewer", function () {
+        test("shows file tree", async () => {
+            await openDocument("fn main() {}", "rust");
+            await vscode.commands.executeCommand("codeBlocks.openTreeViewer");
+
+            const treeViewerDocument = await vscode.workspace.openTextDocument(TreeViewer.uri);
+            expect("\n" + treeViewerDocument.getText()).to.be.equal(`
+source_file [0:0 - 0:12]
+  function_item [0:0 - 0:12]
+    identifier [0:3 - 0:7]
+    parameters [0:7 - 0:9]
+    block [0:10 - 0:12]
+            `);
         });
     });
 });
