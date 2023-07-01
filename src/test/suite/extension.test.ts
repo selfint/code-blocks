0b03154f40188e555d5eecd3917ba551770359e0
selfint <selfint@gmail.com>
2023-07-01 21:09:18 +0300

    refactor test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -55,14 +55,14 @@ source_file [0:0 - 0:12]
     type TestMoveMethodParams = {
         content: string;
         selection: vscode.Selection;
-        command: "codeBlocks.moveDown" | "codeBlocks.moveUp";
+        commands: ("codeBlocks.moveDown" | "codeBlocks.moveUp")[];
         expectedContent: string;
         expectedSelectionContent: string;
     };
-    async function applyMoveMethod({
+    async function testMoveCommands({
         content,
         selection,
-        command,
+        commands,
         expectedContent,
         expectedSelectionContent,
     }: TestMoveMethodParams): Promise<void> {
@@ -71,7 +71,9 @@ source_file [0:0 - 0:12]
         await awaitFileTreeLoaded();
 
         activeEditor.selection = selection;
-        await vscode.commands.executeCommand(command);
+        for (const command of commands) {
+            await vscode.commands.executeCommand(command);
+        }
 
         const newContent = activeEditor.document.getText();
         const newSelectionContent = activeEditor.document.getText(activeEditor.selection);
@@ -80,74 +82,79 @@ source_file [0:0 - 0:12]
         expect(newSelectionContent).to.equal(expectedSelectionContent);
     }
 
-    suite(".moveUp", function () {
-        test("move methods", async () => {
-            await applyMoveMethod({
-                content: "fn main() {} fn foo() { }",
-                selection: new vscode.Selection(new vscode.Position(0, 14), new vscode.Position(0, 23)),
-                command: "codeBlocks.moveUp",
-                expectedContent: "fn foo() { } fn main() {}",
-                expectedSelectionContent: "fn foo() { }",
+    suite("Move", function () {
+        suite(".moveUp", function () {
+            test("moves selection up and updates selection", async () => {
+                await testMoveCommands({
+                    content: "fn main() {} fn foo() { }",
+                    selection: new vscode.Selection(new vscode.Position(0, 14), new vscode.Position(0, 23)),
+                    commands: ["codeBlocks.moveUp"],
+                    expectedContent: "fn foo() { } fn main() {}",
+                    expectedSelectionContent: "fn foo() { }",
+                });
+            });
+
+            test("move in array", async () => {
+                await testMoveCommands({
+                    content: "fn main() {\nlet a = [1, 2, 3, 4, 5]; }",
+                    selection: new vscode.Selection(new vscode.Position(1, 15), new vscode.Position(1, 19)),
+                    commands: ["codeBlocks.moveUp"],
+                    expectedContent: "fn main() {\nlet a = [1, 3, 4, 2, 5]; }",
+                    expectedSelectionContent: "3, 4",
+                });
             });
         });
-    });
 
-    suite(".moveDown", function () {
-        test("moves selection down and updates selection", async () => {
-            await applyMoveMethod({
-                content: "fn main() { }\nfn f() { }",
-                selection: new vscode.Selection(new vscode.Position(0, 1), new vscode.Position(0, 10)),
-                command: "codeBlocks.moveDown",
-                expectedContent: "fn f() { }\nfn main() { }",
-                expectedSelectionContent: "fn main() { }",
+        suite(".moveDown", function () {
+            test("moves selection down and updates selection", async () => {
+                await testMoveCommands({
+                    content: "fn main() { }\nfn f() { }",
+                    selection: new vscode.Selection(new vscode.Position(0, 1), new vscode.Position(0, 10)),
+                    commands: ["codeBlocks.moveDown"],
+                    expectedContent: "fn f() { }\nfn main() { }",
+                    expectedSelectionContent: "fn main() { }",
+                });
             });
         });
-    });
 
-    suite("repeat moves", function () {
-        test("move down/up returns to original", async () => {
-            const activeEditor = await openDocument("fn main() { }\nfn f() { }", "rust");
-            void vscode.commands.executeCommand("codeBlocks.toggle");
-            await awaitFileTreeLoaded();
+        suite("repeat moves", function () {
+            test("move down/up returns to original", async () => {
+                await testMoveCommands({
+                    content: "fn main() { }\nfn f() { }",
+                    selection: new vscode.Selection(new vscode.Position(0, 1), new vscode.Position(0, 10)),
+                    commands: ["codeBlocks.moveDown", "codeBlocks.moveUp"],
+                    expectedContent: "fn main() { }\nfn f() { }",
+                    expectedSelectionContent: "fn main() { }",
+                });
+            });
 
-            activeEditor.selection = new vscode.Selection(
-                new vscode.Position(0, 1),
-                new vscode.Position(0, 10)
-            );
+            test("moving without awaiting is stable", async () => {
+                const activeEditor = await openDocument("fn main() { }\nfn f() { }", "rust");
+                void vscode.commands.executeCommand("codeBlocks.toggle");
+                await awaitFileTreeLoaded();
 
-            await vscode.commands.executeCommand("codeBlocks.moveDown");
-            await vscode.commands.executeCommand("codeBlocks.moveUp");
+                activeEditor.selection = new vscode.Selection(
+                    new vscode.Position(0, 1),
+                    new vscode.Position(0, 10)
+                );
 
-            expect(activeEditor.document.getText()).to.equal("fn main() { }\nfn f() { }");
-            expect(activeEditor.document.getText(activeEditor.selection)).to.equal("fn main() { }");
-        });
+                for (let i = 0; i < 100; i++) {
+                    await Promise.all([
+                        vscode.commands.executeCommand("codeBlocks.moveDown"),
+                        vscode.commands.executeCommand("codeBlocks.moveDown"),
+                        vscode.commands.executeCommand("codeBlocks.moveUp"),
+                        vscode.commands.executeCommand("codeBlocks.moveUp"),
+                    ]);
+                }
 
-        test("moving without awaiting is stable", async () => {
-            const activeEditor = await openDocument("fn main() { }\nfn f() { }", "rust");
-            void vscode.commands.executeCommand("codeBlocks.toggle");
-            await awaitFileTreeLoaded();
-
-            activeEditor.selection = new vscode.Selection(
-                new vscode.Position(0, 1),
-                new vscode.Position(0, 10)
-            );
-
-            for (let i = 0; i < 100; i++) {
-                await Promise.all([
-                    vscode.commands.executeCommand("codeBlocks.moveDown"),
-                    vscode.commands.executeCommand("codeBlocks.moveDown"),
-                    vscode.commands.executeCommand("codeBlocks.moveUp"),
-                    vscode.commands.executeCommand("codeBlocks.moveUp"),
+                // which moves happen first is undefined, but result should
+                // be either of these
+                expect(activeEditor.document.getText()).to.be.oneOf([
+                    "fn main() { }\nfn f() { }",
+                    "fn f() { }\nfn main() { }",
                 ]);
-            }
-
-            // which moves happen first is undefined, but result should
-            // be either of these
-            expect(activeEditor.document.getText()).to.be.oneOf([
-                "fn main() { }\nfn f() { }",
-                "fn f() { }\nfn main() { }",
-            ]);
-            expect(activeEditor.document.getText(activeEditor.selection)).to.equal("fn main() { }");
-        }).timeout(process.env.TEST_TIMEOUT ?? "1m");
+                expect(activeEditor.document.getText(activeEditor.selection)).to.equal("fn main() { }");
+            }).timeout(process.env.TEST_TIMEOUT ?? "1m");
+        });
     });
 });
