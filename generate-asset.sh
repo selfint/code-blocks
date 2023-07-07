ae3a36dff1b7f474ecb0195727ac3a277a7a7ac7
selfint <selfint@gmail.com>
2023-07-08 00:27:03 +0300

    fix ci

--- generate-asset.sh
+++ generate-asset.sh
@@ -10,7 +10,7 @@ assetname=$1
 rawmov="raw.mov"
 
 # record xvfb output
-xvfb-run -s ":99 -ac -screen 0 800x600x24" node ./out/generate-assets/$assetname/runTest.js &
+xvfb-run -s ":99 -ac -screen 0 800x600x24" node ./out/assets/$assetname/runTest.js &
 ffmpeg -y -f x11grab -video_size 800x600 -i :99 -c:v libx264 -pix_fmt yuv420p $rawmov
 
 wait
