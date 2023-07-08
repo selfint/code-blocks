25525bfa748717ada0bea0873d95668e641a3158
selfint <selfint@gmail.com>
2023-07-08 13:03:47 +0300

    simplify example

--- src/examples/suite/example.example.ts
+++ src/examples/suite/example.example.ts
@@ -3,20 +3,11 @@ import { openDocument, sleep } from "../exampleUtils";
 
 const TIMEOUT = process.env.EXAMPLE_TIMEOUT ?? "20s";
 test("Example", async function () {
-    void vscode.window.showInformationMessage("Open any file");
+    void vscode.window.showInformationMessage("Hello world");
     await sleep(1500);
 
     await openDocument(
-        `
-#[derive(Debug)]
-struct A {
-    /// b property
-    b: u32
-}
-
-fn main() {
-
-}
+        `// hello world
 `,
         "rust"
     );
