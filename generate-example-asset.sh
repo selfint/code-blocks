676b00f0e27eb090c54d4af5f7b0a9911866361c
selfint <selfint@gmail.com>
2023-07-09 13:24:33 +0300

    use signal file to signal example start

--- generate-example-asset.sh
+++ generate-example-asset.sh
@@ -16,33 +16,22 @@ output="${output%.*}".gif
 echo "Generating asset for example '$example' into '$output'"
 
 rawmov="raw.mov"
-croppedmov="cropped.mov"
+
+# create test start signal file
+rm -f signal
+touch signal
 
 # run example in xvfb in the background
-EXAMPLE=$example xvfb-run -s ":99 -ac -screen 0 800x600x24" node ./out/examples/runExample.js &
+EXAMPLE=$example xvfb-run -s ":99 -ac -screen 0 800x600x24" node ./out/examples/runExample.js > signal &
+
+# wait for example to start
+while ! grep -q "@" signal; do sleep 0.1; done
 
 # record example into .mov (mov is better than gif for recording)
 ffmpeg -y -f x11grab -video_size 800x600 -i :99 -c:v libx264 -pix_fmt yuv420p $rawmov
 
 wait
 
-# find timestamp of when vscode opens
-ffmpeg -i raw.mov -vf "select='gt(scene,0.01)',metadata=print:file=log.txt" -an -f null -
-start=$(head -n 1 log.txt | awk  -F 'pts_time:' '{ print $2 + 5 }')
-
-cat log.txt
-echo start $start
-
-# remove last second from recording
-newduration=$(ffprobe -v error -show_entries format=duration -of csv=p=0 $rawmov | awk -v s="$start" '{print $1 - s - 1}')
-
-echo start $start
-echo duration $(ffprobe -v error -show_entries format=duration -of csv=p=0 $rawmov)
-echo new duration $newduration
-
-# remove time before test starts and before window closes
-ffmpeg -y -ss $start -i $rawmov -t $newduration $croppedmov
-
 # speedup x2 and convert to gif
-ffmpeg -i $croppedmov -vf "setpts=0.5*PTS" $output
+ffmpeg -i $rawmov -vf "setpts=0.5*PTS" $output
 
