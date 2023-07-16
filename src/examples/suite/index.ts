09e071abadaa9dda1f13f64827445318d4c65198
Tom Selfin <selfint@gmail.com>
2023-07-16 12:07:51 +0300

    remove debug print

--- src/examples/suite/index.ts
+++ src/examples/suite/index.ts
@@ -13,9 +13,6 @@ export function run(): Promise<void> {
     let example = process.env.EXAMPLE;
     example = example.substring(0, example.length - 2) + "js";
 
-    console.log("#".repeat(1000));
-    console.log(example);
-
     // Create the mocha test
     const mocha = new Mocha({
         ui: "tdd",
