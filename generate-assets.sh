388cf54de9e0e332acb6484651f2f10b6b37794f
Tom Selfin <selfint@gmail.com>
2023-07-16 19:07:25 +0300

    try to do parallel creation

--- generate-assets.sh
+++ generate-assets.sh
@@ -9,13 +9,16 @@ examples="$1/suite"
 assets="$2"
 
 echo "Generating assets for examples:"
-ls $examples | grep ".*\.example\.js$"
+ls $examples | grep ".*\.example\.js$" | grep -v "^example.example.js$"
 echo
 
-for example in $(ls $examples | sort | grep ".*\.example\.js$")
-do
+screen=1000
 
-    if [ "$example" != "example.example.js" ] ; then
-        ./generate-example-asset.sh "$example" "$assets"
-    fi
+for example in $(ls $examples | sort | grep ".*\.example\.js$" | grep -v "^example.example.js$")
+do
+    echo "Generating asset for example: '$example' using screen: '$screen'"
+    docker run --rm -v $(pwd):/code-blocks ubuntu:latest /bin/bash -c "apt update && apt upgrade -y && apt install -y ffmpeg libnss3 xvfb && ./generate-example-asset.sh \"$example\" \"$assets\" $screen 2>&1 > \"$example.log\" " &
+    screen=$((screen+1))
 done
+
+wait
--- generate-example-asset.sh
+++ generate-example-asset.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-if [ "$#" -ne 2 ]; then
-        echo "Usage: $0 <example-name> <assets-dir>"
+if [ "$#" -ne 3 ]; then
+        echo "Usage: $0 <example-name> <assets-dir> <screen>"
         exit 1
 fi
 
 example=$1
 assets=$2
+screen=$3
 
 # replace .example.js suffix with .gif
 output="${example%.*}"
 output="$assets/${output%.*}".gif
 
-echo "Generating asset for example '$example' into '$output'"
+echo "Generating asset for example '$example' into '$output' using screen $screen"
 
 mov="$example.mov"
 rm -f "$mov"
@@ -23,20 +24,21 @@ rm -f "$signal"
 touch "$signal"
 
 # run example in xvfb in the background
-EXAMPLE="$example" xvfb-run -s ":99 -ac -screen 0 800x600x24" node ./out/examples/runExample.js > "$signal" &
+EXAMPLE="$example" DISPLAY=":$screen" xvfb-run -n $screen -s ":$screen -ac -screen 0 800x600x24" node ./out/examples/runExample.js > "$signal" &
 
 # wait for example to start
 while ! grep -q "@" "$signal"; do sleep 0.1; done
 
 # record example into .mov (mov is better than gif for recording)
-ffmpeg -y -f x11grab -video_size 800x600 -i :99 -c:v libx264 -pix_fmt yuv420p "$mov"
+ffmpeg -y -f x11grab -video_size 800x600 -i :$screen -c:v libx264 -pix_fmt yuv420p "$mov"
 
 # wait for example to finish, recording will automatically finish (by crashing)
 wait
 
 # speedup and convert to gif
-ffmpeg -i "$mov" -vf "setpts=0.8*PTS" "$output"
+ffmpeg -i "$mov" -vf "setpts=0.5*PTS" "$output"
 
 # cleanup
+cat "$signal"
 rm "$signal" "$mov"
 
