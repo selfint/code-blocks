5b32c461609aa32cb9e41770f52fc556059f17ca
Tom Selfin <selfint@gmail.com>
2024-11-02 19:33:31 +0200

    remove fail-fast from test config

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -7,7 +7,7 @@ export function run(): Promise<void> {
     const mocha = new Mocha({
         ui: "tdd",
         color: true,
-        bail: true,
+        bail: false,
     });
 
     const testsRoot = path.resolve(__dirname, "..");
