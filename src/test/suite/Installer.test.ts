5a4a7e424e841d08e4e73a69c72f3810a6818bcd
Tom Selfin <selfint@gmail.com>
2024-11-02 16:43:17 +0200

    add example content in each example language

--- src/test/suite/Installer.test.ts
+++ src/test/suite/Installer.test.ts
@@ -1,8 +1,11 @@
 import * as Installer from "../../Installer";
+import * as vscode from "vscode";
 
 import Parser from "tree-sitter";
+import { TreeViewer } from "../../TreeViewer";
+import { openDocument } from "./testUtils";
 
-export async function testParser(language: string): Promise<void> {
+export async function testParser(language: string, content?: string): Promise<void> {
     // fail the test if the parser could not be installed
     const result = await Installer.getLanguage("test-parsers", language, true);
     if (result.status === "err") {
@@ -17,40 +20,49 @@ export async function testParser(language: string): Promise<void> {
     } catch (error) {
         throw new Error(`Failed to set language: ${JSON.stringify(error)}`);
     }
+
+    // open a document with the language
+    void openDocument(content ?? `Language: ${language}`, language);
 }
 
 suite("Installer integration tests", function () {
     this.timeout(process.env.TEST_TIMEOUT ?? "1m");
 
+    this.beforeAll(function () {
+        // open file tree viewer for visual debugging
+        void vscode.commands.executeCommand("codeBlocks.openTreeViewer");
+        void vscode.workspace.openTextDocument(TreeViewer.uri);
+    });
+
     const tests = [
-        ["Rust", "rust"],
-        ["TypeScript", "typescript"],
-        ["TSX", "typescriptreact"],
-        ["JavaScript", "javascript"],
-        ["Java", "java"],
-        ["Python", "python"],
-        ["Svelte", "svelte"],
-        ["C", "c"],
-        ["C++", "cpp"],
-        ["C#", "csharp"],
-        ["Go", "go"],
-        ["Ruby", "ruby"],
+        ["Rust", "rust", "fn foo() {}\nfn bar() {}"],
+        ["TypeScript", "typescript", "function foo() {}\nfunction bar() {}"],
+        ["TSX", "typescriptreact", "function Foo(a: string) { return <div />; }\nfunction Bar() {  }"],
+        ["JavaScript", "javascript", "function foo() {}\nfunction bar() {}"],
+        ["Java", "java", "class Foo { public static void main(String[] args) { } }"],
+        ["Python", "python", "def foo():\n    pass\n\ndef bar():\n    pass"],
+        ["Svelte", "svelte", "<script>function foo() {}</script>\n<p>bar</p>"],
+        ["C", "c", "int main() { return 0; }"],
+        ["C++", "cpp", "int main() { return 0; }"],
+        ["C#", "csharp", "class Foo { static void Main() { } }"],
+        ["Go", "go", "package main\n\nfunc main() { }"],
+        ["Ruby", "ruby", "def foo\nend\ndef bar\nend"],
         // ["SQL", "sql"],
-        ["HTML", "html"],
-        ["CSS", "css"],
-        ["YAML", "yaml"],
-        ["JSON", "json"],
+        ["HTML", "html", "<html></html>"],
+        ["CSS", "css", "body { color: red; }"],
+        ["YAML", "yaml", "key: value"],
+        ["JSON", "json", '{ "key": "value" }'],
         // ["XML", "xml"],
-        ["Markdown", "markdown"],
+        ["Markdown", "markdown", "# Title"],
         // ["LaTeX", "latex"],
-        ["Bash", "shellscript"],
+        ["Bash", "shellscript", "echo 'Hello, World!'"],
         // ["TOML", "toml"],
         // ["Swift", "swift"],
-        ["Kotlin", "kotlin"],
-        ["Zig", "zig"],
+        ["Kotlin", "kotlin", "fun main() { }"],
+        ["Zig", "zig", 'const std = @import("std");\n\npub fn main() void { }'],
     ];
 
-    tests.forEach(([name, language]) => {
-        test(name, async () => await testParser(language));
+    tests.forEach(([name, language, content]) => {
+        test(name, async () => await testParser(language, content));
     });
 });
