614d6d0f2662c891fa59e4504847952cf837d322
Tom Selfin <selfint@gmail.com>
2024-11-02 13:11:09 +0200

    build parser only if no pre-built binaries exist

--- src/Installer.ts
+++ src/Installer.ts
@@ -47,24 +47,14 @@ export async function downloadAndBuildParser(
     parsersDir: string,
     parserNpmPackage: string,
     parserName: string,
-    onData?: (data: string) => void,
-    npm = "npm",
-    treeSitterCli = "tree-sitter-cli"
+    npm: string,
+    treeSitterCli: string,
+    onData?: (data: string) => void
 ): Promise<Result<void, string>> {
     // typescript-eslint is wrong, this can return null since we use 'nothrow'
     const npmCommandOk = (await which(npm, { nothrow: true })) !== null;
     if (!npmCommandOk) {
         return err(`npm command: '${npm}' is not in PATH, try installing it from: ${NPM_INSTALL_URL}`);
-    }
-
-    const treeSitterCliOk = await runCmd(`${treeSitterCli} --version`);
-    if (treeSitterCliOk.status === "err") {
-        return err(
-            `
-            tree-sitter cli command '${treeSitterCli}' failed:
-            ${treeSitterCliOk.result[0].name} ${treeSitterCliOk.result[0].message.replace(/\n/g, " > ")}.` +
-                (treeSitterCliOk.result[1].length > 1 ? ` Logs: ${treeSitterCliOk.result[1].join(">")}` : "")
-        );
     }
 
     const parserDir = getAbsoluteParserDir(parsersDir, parserName);
@@ -96,6 +86,46 @@ export async function downloadAndBuildParser(
     } catch (e: unknown) {
         onData?.(`failed to extract ${tarFilename} to ${parserDir} > ${JSON.stringify(e)}`);
         return err(`failed to extract ${tarFilename} to ${parserDir} > ${JSON.stringify(e)}`);
+    }
+
+    // try to load parser, without building it
+    let loadResult = await loadParser(parsersDir, parserName);
+    if (loadResult.status === "ok") {
+        return ok(undefined);
+    }
+
+    // we need to build the parser
+    const treeSitterCliOk = await runCmd(`${treeSitterCli} --version`);
+    if (treeSitterCliOk.status === "ok") {
+        // build parser using tree-sitter cli
+        const buildResult = await runCmd(`${treeSitterCli} generate`, { cwd: parserDir }, onData);
+        if (buildResult.status === "err") {
+            const cliMsg =
+                buildResult.result[0].name +
+                ": " +
+                buildResult.result[0].message.replace(/\n/g, " > ") +
+                (buildResult.result[1].length > 1 ? ` Logs: ${buildResult.result[1].join(">")}` : "");
+
+            return err(`Failed to build parser using tree-sitter cli > ${cliMsg}`);
+        }
+    } else {
+        const cliMsg =
+            treeSitterCliOk.result[0].name +
+            ": " +
+            treeSitterCliOk.result[0].message.replace(/\n/g, " > ") +
+            (treeSitterCliOk.result[1].length > 1 ? ` Logs: ${treeSitterCliOk.result[1].join(">")}` : "");
+
+        return err(
+            `
+            Parser '${parserName}' requires local build, but 
+            tree-sitter cli command '${treeSitterCli} --version' failed: ${cliMsg}.
+            `
+        );
+    }
+
+    loadResult = await loadParser(parsersDir, parserName);
+    if (loadResult.status === "err") {
+        return err(`Failed to load parser after building it > ${loadResult.result}`);
     }
 
     return ok(undefined);
@@ -188,9 +218,9 @@ export async function getLanguage(
                     parsersDir,
                     npmPackageName,
                     parserName,
-                    (data) => progress.report({ message: data, increment: number++ }),
                     npm,
-                    treeSitterCli
+                    treeSitterCli,
+                    (data) => progress.report({ message: data, increment: number++ })
                 );
             }
         );
