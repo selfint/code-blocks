c1ba2d102ecde145ee21178553849cd68a7d88a5
Tom Selfin <selfint@gmail.com>
2024-11-02 13:11:45 +0200

    add tests for Installer

--- src/test/suite/Installer.test.ts
+++ src/test/suite/Installer.test.ts
@@ -1,76 +1,56 @@
 import * as Installer from "../../Installer";
-import * as vscode from "vscode";
 
-import { BlockMode, active, activeFileTree } from "../../extension";
+import Parser from "tree-sitter";
 
-import { FileTree } from "../../FileTree";
-import { expect } from "chai";
-
-export async function testParser(
-    language: string,
-    content = ""
-): Promise<{ activeEditor: vscode.TextEditor; fileTree: FileTree }> {
-    if (!active.get()) {
-        active.set(true);
-    }
-
-    if (!BlockMode.blockModeActive.get()) {
-        BlockMode.toggleBlockMode();
-    }
-
-    // install the parser
+export async function testParser(language: string): Promise<void> {
+    // fail the test if the parser could not be installed
     const result = await Installer.getLanguage("test-parsers", language, true);
-
     if (result.status === "err") {
-        // fail the test if the parser could not be installed
-        throw new Error(`Failed to install parser: ${result.result}`);
+        throw new Error(`Failed to install language: ${result.result}`);
     }
 
-    const activeEditor = await vscode.window.showTextDocument(
-        await vscode.workspace.openTextDocument({
-            language,
-            content,
-        })
-    );
+    // check the language can be set
+    const parser = new Parser();
 
-    let fileTree = activeFileTree.get();
-    if (fileTree === undefined) {
-        fileTree = await new Promise<FileTree>((r) => {
-            activeFileTree.onDidChange((fileTree) => {
-                if (fileTree !== undefined) {
-                    r(fileTree);
-                }
-            });
-        });
+    try {
+        parser.setLanguage(result.result);
+    } catch (error) {
+        throw new Error(`Failed to set language: ${JSON.stringify(error)}`);
     }
-
-    return { activeEditor, fileTree };
 }
 
 suite("Installer integration tests", function () {
     this.timeout(process.env.TEST_TIMEOUT ?? "1m");
 
-    suite("Rust", function () {
-        test("Hello world", async function () {
-            const { activeEditor, fileTree } = await testParser(
-                "rust",
-                'fn main() {\n    println!("Hello, world!");\n}'
-            );
+    const tests = [
+        ["Rust", "rust"],
+        ["TypeScript", "typescript"],
+        ["TSX", "typescriptreact"],
+        ["JavaScript", "javascript"],
+        ["Java", "java"],
+        ["Python", "python"],
+        ["Svelte", "svelte"],
+        ["C", "c"],
+        ["C++", "cpp"],
+        ["C#", "csharp"],
+        ["Go", "go"],
+        ["Ruby", "ruby"],
+        // ["SQL", "sql"],
+        ["HTML", "html"],
+        ["CSS", "css"],
+        ["YAML", "yaml"],
+        ["JSON", "json"],
+        // ["XML", "xml"],
+        ["Markdown", "markdown"],
+        // ["LaTeX", "latex"],
+        ["Bash", "shellscript"],
+        // ["TOML", "toml"],
+        // ["Swift", "swift"],
+        ["Kotlin", "kotlin"],
+        ["Zig", "zig"],
+    ];
 
-            expect(activeEditor.document.languageId).to.equal("rust");
-            expect(fileTree).to.not.be.undefined;
-        });
-    });
-
-    suite("Java", function () {
-        test("Hello world", async function () {
-            const { activeEditor, fileTree } = await testParser(
-                "java",
-                'public class Main {\n    public static void main(String[] args) {\n        System.out.println("Hello, world!");\n    }\n}'
-            );
-
-            expect(activeEditor.document.languageId).to.equal("java");
-            expect(fileTree).to.not.be.undefined;
-        });
+    tests.forEach(([name, language]) => {
+        test(name, async () => await testParser(language));
     });
 });
