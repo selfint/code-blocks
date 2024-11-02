97093cfb77178fc0c718f6ce64ea3a805a28250c
Tom Selfin <selfint@gmail.com>
2024-11-02 21:31:46 +0200

    remove console.log channel wrapper
    doesn't work

--- src/outputChannel.ts
+++ src/outputChannel.ts
@@ -7,22 +7,13 @@ type Logger = {
 };
 
 export function getLogger(): Logger {
-    // TODO: hack to support logging in and out of vscode context
-    try {
-        if (channel === undefined) {
-            channel = vscode.window.createOutputChannel("CodeBlocks");
-        }
-
-        return {
-            log: (message: string): void => {
-                channel?.appendLine(message);
-            },
-        };
-    } catch (error) {
-        return {
-            log: (message: string): void => {
-                console.log(message);
-            },
-        };
+    if (channel === undefined) {
+        channel = vscode.window.createOutputChannel("CodeBlocks");
     }
+
+    return {
+        log: (message: string): void => {
+            channel?.appendLine(message);
+        },
+    };
 }
