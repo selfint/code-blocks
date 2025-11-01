6ff4cd9adc562d295f96b4cfa0e16acb7aa66fc4
Tom Selfin <selfint@gmail.com>
2025-11-01 11:06:34 +0200

    update block move rust match arms example

--- src/examples/suite/Block_Mode_-_Move_-_Rust_-_Match_arms.example.ts
+++ src/examples/suite/Block_Mode_-_Move_-_Rust_-_Match_arms.example.ts
@@ -4,11 +4,16 @@ const TIMEOUT = process.env.EXAMPLE_TIMEOUT ?? "2m";
 test("Block mode 2", async function () {
     await moveExample({
         language: "rust",
-        content: `fn main() {
-    match rand::thread_rng().gen_range(0..=3) {
+        content: `\
+fn main() {
+    match rand::thread_rng().gen_range(0..=7) {
         0 => println!("hi"),
         1 => println!("he@llo"),
         2 => println!("howdy"),
+        3 => println!("howdy hoo"),
+        4 => println!("howdy there"),
+        5 => println!("he@llo there"),
+        6 => println!("goodbye"),
         _ => println!("salutations"),
     }
 }`,
@@ -17,17 +22,27 @@ test("Block mode 2", async function () {
             "codeBlocks.selectParent",
             "codeBlocks.selectParent",
             "codeBlocks.selectParent",
+            "codeBlocks.selectParent",
             "codeBlocks.selectPrevious",
         ],
         selectionMessage: "Select match arms",
         moveCommands: ["codeBlocks.moveDown", "codeBlocks.moveDown", "codeBlocks.moveUp"],
-        expectedSelectionContent: `0 => println!("hi"),
+        expectedSelectionContent: [
+            `0 => println!("hi"),
         1 => println!("hello"),`,
-        expectedContent: `fn main() {
-    match rand::thread_rng().gen_range(0..=3) {
+            `4 => println!("howdy there"),
+        5 => println!("hello there"),`,
+        ],
+        expectedContent: `\
+fn main() {
+    match rand::thread_rng().gen_range(0..=7) {
         2 => println!("howdy"),
         0 => println!("hi"),
         1 => println!("hello"),
+        3 => println!("howdy hoo"),
+        6 => println!("goodbye"),
+        4 => println!("howdy there"),
+        5 => println!("hello there"),
         _ => println!("salutations"),
     }
 }`,
